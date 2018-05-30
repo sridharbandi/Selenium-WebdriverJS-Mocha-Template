e49fc5e1f245afd43eedabd76a46043cde5d8532
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-05-30 22:42:38 +0100

    Added Driverutil

--- tests/tests.js
+++ tests/tests.js
@@ -1,29 +1,32 @@
-import "chromedriver";
+import GoogleSearchPage from '../pageobjects/googlehomepage';
+import SearchResultsPage from '../pageobjects/searchresultspage';
 import {assert} from 'chai';
-import {Builder, By, until} from "selenium-webdriver";
-{describe, before, after, it} import "selenium-webdriver/testing";
+import {argv} from 'yargs';
+import getDriver from '../driverutil/driverutil';
+{
+    describe, before, after, it
+}
+import "selenium-webdriver/testing";
 
-describe('Google Search', function() {
+describe('Google Search', function () {
     let driver;
     let googlesearchpage;
     let searchresultspage;
     this.timeout(50000);
 
-    before(() => {
-        return new Builder().forBrowser('chrome').build().then(d => {
-            driver = d;
-            googlesearchpage = require('../pageobjects/googlehomepage')(driver);
-            searchresultspage = require('../pageobjects/searchresultspage')(driver);
-        });
+    before(async () => {
+        driver = await getDriver(argv.browser)
+        googlesearchpage = new GoogleSearchPage(driver);
+        searchresultspage = new SearchResultsPage(driver);
     });
 
-    it('Google Search Test',async function() {
+    it('Google Search Test', async function () {
         await googlesearchpage.navigate();
         await googlesearchpage.searchfor('Selenium');
         await searchresultspage.waitUntilPageLoaded();
-        assert.isTrue( await searchresultspage.assertlinkpresent(),'Selenium Link Present');
+        assert.isTrue(await searchresultspage.assertlinkpresent(), 'Selenium Link Present');
     });
 
-    after(()=>driver.quit());
+    after(() => driver.quit());
 });
 
